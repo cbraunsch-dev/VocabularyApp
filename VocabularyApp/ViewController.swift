5e9f197228e3512b9b884a9bbfd81e41f0c1b62a
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2019-02-10 15:03:33 +0100

    Initial Commit

---
+++ VocabularyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocabularyApp
+//
+//  Created by Chris Braunschweiler on 10.02.19.
+//  Copyright © 2019 braunsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
